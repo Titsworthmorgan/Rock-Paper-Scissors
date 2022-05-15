8b1c7ea035863baf474c5b599e224063bf24b5fd
Titsworth <morgan@tobedetermined.dev>
2022-05-14 23:53:24 -0400

    asd

--- external.js
+++ external.js
@@ -1,94 +1,73 @@
-// function resetGame () {
-//     const reset =document.quary 
-// }
+let computerSelection = Math.floor(Math.random() * 3)
+let playerChoice;
+let outcome;
+let userScore = 0;
+let opponentScore = 0;
 
+function play(choice){
+     playerChoice = choice;
+     outcome = gameRound();
+     game();
+}
 
+function gameRound(){
+    let compChoice = computerSelection;
+    
 
+    switch(playerChoice) {
+    case compChoice: {
+        return 'tie';
+    }
+    case 1: {
+        switch(compChoice){
+            case 0:{
+                return 'win';
+            }
+            case 2:{
+                return 'lose';
+            }
+        }
+    }
+    case 0: {
+        switch(compChoice){
+            case 2:{
+                return 'win';
+            }
+            case 1:{
+                return 'lose';
+            }
+        }
+    }
+    case 2: {
+        switch(compChoice){
+            case 0:{
+                return 'lose';
+            }
+            case 1:{
+                return 'win';
+            }
+        }
+    }
+    default: {
+        console.log("incorrect Entry")
+        break;
+        }
+    }
+}
 
-// // -- Old code --
+function game(){
+        if (outcome === 'win'){
+            userScore++;
+        } else if (outcome === 'lose'){
+            opponentScore++
+        }if (userScore == 5) {
+        console.log('user wins');
+        } else if (opponentScore == 5){
+        console.log('computer wins');
+    } 
+}
 
-// const OPTIONS = ['rock', 'paper', 'scissors'];
-// let selection = OPTIONS[Math.floor(Math.random() * 3)];
-// const images = document.querySelectorAll('image');
-// const click = document.querySelectorAll('image');
-// images.forEach(click => click.addEventListener('click'));
-
-// function selector(e) {
-
-// }
-
-
-// function gameRound(){
-//     let compChoice = selection;
-//     let playerChoice = window.addEventListener()
-
-//     switch(playerChoice) {
-//     case compChoice: {
-//         return 'tie';
-//     }
-//     case "paper": {
-//         switch(compChoice){
-//             case "rock":{
-//                 return 'win';
-//             }
-//             case "scissors":{
-//                 return 'lose';
-//             }
-//         }
-//         break;
-//     }
-//     case "rock": {
-//         switch(compChoice){
-//             case "scissors":{
-//                 return 'win';
-//             }
-//             case "paper":{
-//                 return 'lose';
-//             }
-//         }
-//         break;
-//     }
-//     case "scissors": {
-//         switch(compChoice){
-//             case "rock":{
-//                 return 'lose';
-//             }
-//             case "paper":{
-//                 return 'win';
-//             }
-//         }
-//         break;
-//     }
-//     default: {
-//         console.log("incorrect Entry")
-//         break;
-//         }
-//     }
-// }
-
-// function game() {
-//     let outcome; 
-//     let userScore = 0;
-//     let opponenScore = 0;
-
-//     for (let i = 0; i < 5; i++) {
-//         outcome = gameRound();
-
-//         if (outcome === 'win') {
-//             userScore++;
-//         } else if (outcome === 'lose'){
-//             opponenScore++
-//         }
-//     }
-//     if (userScore === opponenScore) {
-//         console.log('tie game');
-//     } else if (userScore > opponenScore) {
-//         console.log('you win') 
-//     } else if (userScore < opponenScore) {
-//         console.log('you lose')
-//     } 
-// }
-
-// game();
-
-// // -- end old code --
+function resetGame(){
+    let userScore = 0;
+    let opponentScore = 0;
+}
